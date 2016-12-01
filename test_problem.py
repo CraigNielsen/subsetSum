93b8992c6643fe70984fbaff9ebe84536379b7b5
Craig Nielsen <craiglbn@gmail.com>
2016-12-01 11:26:21 +0200

    Ome edites

--- test_problem.py
+++ test_problem.py
@@ -4,6 +4,16 @@ import numpy as np
 dataset =[18897109, 12828837, 9661105, 6371773, 5965343, 5926800, 5582170, 5564635, 5268860, 4552402, 4335391, 4296250, 4224851, 4192887, 3439809, 3279933, 3095213, 2812896, 2783243, 2710489, 2543482, 2356285, 2226009, 2149127, 2142508, 2134411]
 required_total = 101000000
 
+''' minor optimization for this dataset:
+        (1) answer must contain 1st or 2cd element of array because: the sum of dataset[2:] < required total
+        so can run subset sum without the first element, and attempt to find a solution (required_total = required_total - first element)
+        if solution is found, can add dataset[0] to found subset solution to get the set that sums to the required total
+
+        - if a solution set is not found, you can be sure the second element of the array is part of the subset, (due to (1))
+        can run the subsetSum() with required_total - both first and second elements.
+        add second element to final subset solution.
+    '''
+
 def sumList(llist):
     return reduce(lambda x,y: x+y, llist)
 def test_sumList():
@@ -48,15 +58,6 @@ def test_SubsetSum():
     assert subsetSum([2,3,7,8,10], 15) == (True, [8,7])
     assert subsetSum([2,3,7,8,109], 15) == (True, [8,7])
 
-    ''' minor optimization for this dataset:
-        (1) answer must contain 1st or 2cd element of array because: the sum of dataset[2:] < required total
-        so can run subset sum without the first element, and attempt to find a solution (required_total = required_total - first element)
-        if solution is found, can add dataset[0] to found subset solution to get the set that sums to the required total
-
-        - if a solution set is not found, you can be sure the second element of the array is part of the subset, (due to (1))
-        can run the subsetSum() with required_total - both first and second elements.
-        add second element to final subset solution.
-    '''
     total = required_total-dataset[0]
     has_subset, solution = subsetSum(dataset[1:], total)
     if has_subset:
@@ -68,5 +69,5 @@ def test_SubsetSum():
         solution.append(dataset[1])
         print("the solution is: ", solution)
 
-    # solution=  [18897109,2134411, 2142508, 2149127, 2226009, 2543482, 2710489, 2812896, 3095213, 3279933, 4224851, 4296250, 4552402, 5564635, 5582170, 5926800, 6371773, 9661105, 12828837]
-    print(sumList(solution))
+    # solution=  [18897109, 2134411, 2142508, 2149127, 2226009, 2543482, 2710489, 2812896, 3095213, 3279933, 4224851, 4296250, 4552402, 5564635, 5582170, 5926800, 6371773, 9661105, 12828837]
+    print("final solution sum: ", sumList(solution))
