1a318b6eabed113a48c067d1d7065c448492100b
Craig Nielsen <craiglbn@gmail.com>
2016-12-01 10:06:32 +0200

    Add edits

--- test_problem.py
+++ test_problem.py
@@ -1,10 +1,9 @@
 from functools import reduce
 import pytest
 import numpy as np
-from math import ceil
 import pdb
 
-al =[18897109, 12828837, 9661105, 6371773, 5965343, 5926800, 5582170, 5564635, 5268860, 4552402, 4335391, 4296250, 4224851, 4192887, 3439809, 3279933, 3095213, 2812896, 2783243, 2710489, 2543482, 2356285, 2226009, 2149127, 2142508, 2134411]
+dataset =[18897109, 12828837, 9661105, 6371773, 5965343, 5926800, 5582170, 5564635, 5268860, 4552402, 4335391, 4296250, 4224851, 4192887, 3439809, 3279933, 3095213, 2812896, 2783243, 2710489, 2543482, 2356285, 2226009, 2149127, 2142508, 2134411]
 y = 101000000
 
 def sumList(llist):
@@ -21,22 +20,14 @@ def subsetSum(array, total):
     for i in range(len(array) ):
         for j in range(1, total+1 ):
             if i==0:
-                if j<array[0]:
-                    B[0][j]=False
-                elif j>array[0]: B[0][j]=False
-            # if i==2 and j==2:
-                # pdb.set_trace()
+                B[0][j]=False
             elif (j - array[i] >= 0):
                 B[i][j] = B[i-1][j] or B[i-1][j - array[ i ]]
             else:
                 B[i][j] = B[i-1][j]
             if j==array[i]: B[i][j]=True
-    # assert np.array_equal(B ,
-            # [[ True ,False  ,True ,False ,False ,False ,False ,False ,False ,False ,False ,False],#2
-             # [ True ,False  ,True  ,True ,False  ,True ,False ,False ,False ,False ,False ,False],#3
-             # [ True ,False  ,True  ,True ,False  ,True ,False  ,True ,False  ,True  ,True ,False],#7
-             # [ True ,False  ,True  ,True ,False  ,True ,False  ,True  ,True  ,True  ,True  ,True],#8
-             # [ True ,False  ,True  ,True ,False  ,True ,False  ,True  ,True  ,True  ,True  ,True]])#10
+
+#   trace solution
     solution = []
     i = len(array) - 1
     k = total
@@ -60,19 +51,25 @@ def test_SubsetSum():
     assert subsetSum([2,3,7,8,10], 18) == (True, [8,7,3])
     assert subsetSum([2,3,7,8,10], 15) == (True, [8,7])
     assert subsetSum([2,3,7,8,109], 15) == (True, [8,7])
-    print(subsetSum([2,3,7,8,109], 15)[0])
 
-    sum_all = sumList(al[1:])
-    #must contain 0 or 1st element
-    total = y-al[0]
-    has_subset, solution = subsetSum(al[1:], total)
+    sum_all = sumList(dataset[1:])
+    ''' minor optimization for this dataset:
+        answer must contain 1st or 2cd element of array: the sum of dataset[2:] < required total
+        so can run subset sum without the first element, and attempt to find a solution(minus first element from required_total)
+        if solution is found, can add dataset[0] to found solution to get set that sums to required total
+        if a solution is not found, you can be sure the second element of the array is part of the subset,
+        and run the subsetSum() with for required_total - both first and second elements.
+        your final subset will include the second element of given array.
+    '''
+    total = y-dataset[0]
+    has_subset, solution = subsetSum(dataset[1:], total)
     if has_subset == True:
-        solution.append(al[0])
+        solution.append(dataset[0])
         print("the solution is: ", solution)
     else:
-        total = y-al[0]-al[1]
-        has_subset, solution = subsetSum(al[2:], total)
-        solution.append(al[1])
+        total = y-dataset[0]-dataset[1]
+        has_subset, solution = subsetSum(dataset[2:], total)
+        solution.append(dataset[1])
         print("the solution is: ", solution)
 
     # solution=  [18897109,2134411, 2142508, 2149127, 2226009, 2543482, 2710489, 2812896, 3095213, 3279933, 4224851, 4296250, 4552402, 5564635, 5582170, 5926800, 6371773, 9661105, 12828837]
